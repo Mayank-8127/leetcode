e33b3d3ec4639e283ef5ab725e2f432d1cd06e84
Mayank Bansal <145133998+Mayank-8127@users.noreply.github.com>
2025-08-19 20:39:04 +0530

    Create 118-PascalsTriangle

---
+++ 118-PascalsTriangle/118-PascalsTriangle.cpp
@@ -0,0 +1,20 @@
+// Last updated: 8/19/2025, 8:39:06 PM
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> arr;
+        for(int i = 0; i < numRows; i++){
+            vector<int> temp;
+            for(int j = 0; j <= i; j++){
+                if(j == 0 || j == i){
+                    temp.push_back(1);
+                }
+                else{
+                    temp.push_back(arr[i-1][j-1] + arr[i-1][j]);
+                }
+            }
+            arr.push_back(temp);
+        }
+        return arr;
+    }
+};
