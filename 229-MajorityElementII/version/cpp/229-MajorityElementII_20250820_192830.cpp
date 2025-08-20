4ce2328499048d5024bcd3a06645a3a6a43a1f09
Mayank Bansal <145133998+Mayank-8127@users.noreply.github.com>
2025-08-20 19:28:28 +0530

    Create 229-MajorityElementII

---
+++ 229-MajorityElementII/version/cpp/229-MajorityElementII_20250820_192830.cpp
@@ -0,0 +1,58 @@
+// Last updated: 8/20/2025, 7:28:30 PM
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size(), cnt1 = 0, cnt2 = 0, num1, num2;
+        vector<int> arr;
+        for(int i = 0; i < n; i++){
+            if(cnt1 == 0 && nums[i] != num2){
+                num1 = nums[i];
+                cnt1 = 1;
+            }else if(cnt2 == 0 && nums[i] != num1){
+                num2 = nums[i];
+                cnt2 = 1;
+            }else if(nums[i] == num1){
+                cnt1++;
+            }else if(nums[i] == num2){
+                cnt2++;
+            }else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == num1) cnt1++;
+            else if(nums[i] == num2) cnt2++;
+        }
+        if(cnt1 > n/3) arr.push_back(num1);
+        if(cnt2 > n/3) arr.push_back(num2);
+        return arr;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+};
