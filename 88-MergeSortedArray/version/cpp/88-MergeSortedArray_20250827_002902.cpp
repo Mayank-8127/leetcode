f1212a9cd26756890820c2baa43492a978d72ce5
Mayank Bansal <145133998+Mayank-8127@users.noreply.github.com>
2025-08-27 00:29:03 +0530

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/version/cpp/88-MergeSortedArray_20250827_002902.cpp
@@ -0,0 +1,26 @@
+// Last updated: 8/27/2025, 12:29:02 AM
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i = m+n-1; i > n-1; i--){
+            nums1[i] = nums1[i-n];
+        }
+        int p1 = n, p2 = 0;
+        for(int i = 0; i < m+n; i++){
+            if(p1 > m+n-1){
+                nums1[i] = nums2[p2];
+                p2++;
+            }else if(p2 > n-1){
+                nums1[i] = nums1[p1];
+                p1++;
+            }else if(nums1[p1] <= nums2[p2]){
+                nums1[i] = nums1[p1];
+                p1++;
+            }else{
+                nums1[i] = nums2[p2];
+                p2++;
+            }
+        }
+
+    }
+};
