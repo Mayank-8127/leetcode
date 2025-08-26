5d3b5ad63ca8a748bae05791b531e2d176f7e27a
Mayank Bansal <145133998+Mayank-8127@users.noreply.github.com>
2025-08-27 05:24:43 +0530

    Create 152-MaximumProductSubarray

---
+++ 152-MaximumProductSubarray/version/cpp/152-MaximumProductSubarray_20250827_052442.cpp
@@ -0,0 +1,17 @@
+// Last updated: 8/27/2025, 5:24:42 AM
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int mx = -1000000;
+        int pre = 1, suff = 1;
+        for(int i = 0; i < n; i++){
+            if(pre == 0) pre = 1;
+            if(suff == 0) suff = 1;
+            pre = pre*nums[i];
+            suff = suff*nums[n-i-1];
+            mx = max(mx, max(pre, suff));
+        }
+        return mx;
+    }
+};
