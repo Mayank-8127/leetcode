0fdaa5ee2efc24bef6e66f8034b0fe59d65830ab
Mayank Bansal <145133998+Mayank-8127@users.noreply.github.com>
2025-08-28 02:37:53 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cpp
@@ -0,0 +1,22 @@
+// Last updated: 8/28/2025, 2:37:51 AM
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s = 0, e = nums.size() - 1;
+        int m = (s+e)/2;
+        while(m > s && m < e){
+            if(target > nums[m]){
+                s = m;
+                m = (s+e)/2;
+            }else if(target < nums[m]){
+                e = m;
+                m = (s+e)/2;
+            }else{
+                return m;
+            }
+        }
+        if(nums[m] == target) return m;
+        if(nums[e] == target) return e;
+        return -1;
+    }
+};
