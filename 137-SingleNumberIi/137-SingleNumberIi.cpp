650f51e6cbf92989062b4cdeee39c74984be280c
Mayank Bansal <145133998+Mayank-8127@users.noreply.github.com>
2025-10-17 02:58:56 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.cpp
@@ -0,0 +1,13 @@
+// Last updated: 10/17/2025, 2:58:54 AM
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0;
+        int twos = 0;
+        for(int i = 0; i < nums.size(); i++){
+            ones = (ones^nums[i]) & ~twos;
+            twos = (twos^nums[i]) & ~ones;
+        }
+        return ones;
+    }
+};
