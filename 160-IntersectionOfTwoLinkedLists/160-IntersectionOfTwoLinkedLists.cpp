61ede6ef953d247328cf557084170a7d750c6948
Mayank Bansal <145133998+Mayank-8127@users.noreply.github.com>
2026-02-07 01:20:29 +0530

    Create 160-IntersectionOfTwoLinkedLists

---
+++ 160-IntersectionOfTwoLinkedLists/160-IntersectionOfTwoLinkedLists.cpp
@@ -0,0 +1,52 @@
+// Last updated: 2/7/2026, 1:20:30 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *temp1 = headA, *temp2 = headB;
+        // while(temp1 != NULL && temp2 != NULL && temp1 != temp2){
+        //     temp1 = temp1->next;
+        //     temp2 = temp2->next;
+        // }
+        // if(temp1 == temp2){
+        //     return temp1;
+        // }
+        // if(temp1 == NULL) temp1 = headB;
+        // if(temp2 == NULL) temp2 = headA;
+        // while(temp1 != NULL && temp2 != NULL){
+        //     temp1 = temp1->next;
+        //     temp2 = temp2->next;
+        // }
+        // if(temp1 == NULL) temp1 = headB;
+        // if(temp2 == NULL) temp2 = headA;
+        // while(temp1 != NULL && temp2 != NULL && temp1 != temp2){
+        //     temp1 = temp1->next;
+        //     temp2 = temp2->next;
+        // }
+        // if(temp1 == temp2){
+        //     return temp1;
+        // }else{
+        //     return NULL;
+        // }
+        while(true){
+            if(temp1 == temp2) return temp1;
+
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+
+            if(temp1 == temp2) return temp1;
+            
+            if(temp1 == NULL && temp2 == NULL) return NULL;
+
+            if(temp1 == NULL) temp1 = headB;
+            if(temp2 == NULL) temp2 = headA;
+        }
+    }
+};
